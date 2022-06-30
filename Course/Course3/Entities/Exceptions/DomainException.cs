26d277ef0f8c752bad5abb54496bb17ee58fa7da
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-06-30 16:05:12 -0300

    Exceptions

---
+++ Course/Course3/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Course3.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Course/Course3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Course3.Entities;
+using Course3.Entities.Exceptions;
+
+namespace Course3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-In (DD/MM/YYYY): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out (DD/MM/YYYY): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(room, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("\nEnter data to update the reservation:");
+                Console.Write("Check-In (DD/MM/YYYY): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out (DD/MM/YYYY): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation!: " + e.Message);
+            }
+
+        }
+    }
+}
